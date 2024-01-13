37073f47327afb74ac220d61001b0eed529e9535
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-13 18:06:26 +0800

    chore: add webpack for testing

--- rsbuild.config.webpack.mjs
+++ rsbuild.config.webpack.mjs
@@ -1,15 +1,14 @@
 import { defineConfig } from "@rsbuild/core";
+import { webpackProvider } from "@rsbuild/webpack";
+import { pluginSwc } from "@rsbuild/plugin-swc";
 
 export default defineConfig({
+  provider: webpackProvider,
+  plugins: [pluginSwc()],
   tools: {
     rspack: {
       optimization: {
         splitChunks: false,
-      },
-      experiments: {
-        rspackFuture: {
-          newTreeshaking: true,
-        },
       },
     },
   },
